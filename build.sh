988684d642129915a1686655adbdc61e050047f6
ssgsqx <sqx1204903742@qq.com>
2020-11-12 16:13:02 +0800

    del useless

--- build.sh
+++ build.sh
@@ -3,34 +3,6 @@
 
 echo "env=$1"
 
-# REACT_APP_RTC_HOST="https://a1.easemob.com"
-# REACT_APP_IM_HOST="https://a1.easemob.com"
-# REACT_APP_WHITE_BOARD_HOST="https://wbrtc.easemob.com"
-
-# if [ $1 = "prod" ] 
-# then
-#     REACT_APP_RTC_HOST="https://a1.easemob.com"
-#     REACT_APP_IM_HOST="https://a1.easemob.com"
-#     REACT_APP_WHITE_BOARD_HOST="https://wbrtc.easemob.com"
-# elif [ $1 = "test" ] 
-# then
-#     REACT_APP_RTC_HOST='https://a1-hsb.easemob.com'
-#     REACT_APP_IM_HOST='https://a1-hsb.easemob.com'
-#     REACT_APP_WHITE_BOARD_HOST='https://wbrtc-hsb.easemob.com'
-# elif [ $1 = "turn4" ] 
-# then
-#     REACT_APP_RTC_HOST='https://rtc-turn4-hsb.easemob.com'
-#     REACT_APP_IM_HOST='https://a1-hsb.easemob.com'
-#     REACT_APP_WHITE_BOARD_HOST='https://wbrtc-hsb.easemob.com'
-# fi
-
-
-
-# process.env.REACT_APP_IM_HOST=$REACT_APP_IM_HOST
-# process.env.REACT_APP_RTC_HOST=$REACT_APP_RTC_HOST
-# process.env.REACT_APP_WHITE_BOARD_HOST=$REACT_APP_WHITE_BOARD_HOST
-
-
 react-app-rewired build
 
 build_name=''
