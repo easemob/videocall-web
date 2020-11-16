7f2d181e44a05b3ac62efa65e0623e895b989781
ssgsqx <sqx1204903742@qq.com>
2020-11-16 12:06:36 +0800

    build pag

---
+++ build/precache-manifest.d459058051e4e912e2046921e845c6fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18efa64a76cef1786966c4ccdd5a666c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2e327e4a0858edff975",
+    "url": "./static/css/2.3ffdace4.chunk.css"
+  },
+  {
+    "revision": "b6f92e3366154d485370",
+    "url": "./static/css/main.99145032.chunk.css"
+  },
+  {
+    "revision": "d2e327e4a0858edff975",
+    "url": "./static/js/2.56e028d6.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "./static/js/2.56e028d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f92e3366154d485370",
+    "url": "./static/js/main.dd799618.chunk.js"
+  },
+  {
+    "revision": "9823f9ba412559863161",
+    "url": "./static/js/runtime-main.fdda5974.js"
+  },
+  {
+    "revision": "35ac64e9dff3f989f36125458f31cb25",
+    "url": "./static/media/login-bg.35ac64e9.png"
+  }
+]);
